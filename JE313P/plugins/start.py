36398df10c9518945d134d5dbababfb64294628a
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2023-11-27 20:39:24 +0300

    Update start.py

--- JE313P/plugins/start.py
+++ JE313P/plugins/start.py
@@ -41,7 +41,7 @@ async def _(event):
     if event.is_private:
        await event.edit(PM_START_TEXT.format(event.sender.first_name), buttons=[
         [Button.url("➕ اضغط هنا لاضافتي", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-        [Button.url("السورس", "https://t.me/JEPTHON")],
+        [Button.url("السورس", "https://t.me/SU_OP")],
         [Button.url("المطور", f"https://t.me/{Config.SUPPORT}"), Button.url("القناة", f"https://t.me/{Config.CHANNEL}")],
         [Button.inline("الاوامر", data="help")]])
        return
